a5e3583a66955da6b83511072c23fd3336581d30
Ikjot Singh Dhody <ikjotsd@gmail.com>
2022-03-26 21:08:40 +0530

    Connected React app with IPFS for file upload - retrieve CID

---
+++ src/App.js
@@ -0,0 +1,38 @@
+/* src/App.js */
+import './App.css'
+import { useState } from 'react'
+import { create } from 'ipfs-http-client'
+
+const client = create('https://ipfs.infura.io:5001/api/v0')
+
+function App() {
+  const [fileUrl, updateFileUrl] = useState(``)
+  async function onChange(e) {
+    const file = e.target.files[0]
+    try {
+      const {cid} = await client.add(file)
+      var cid_value = cid._baseCache.get('z')
+      console.log(cid_value)
+      const url = `https://ipfs.infura.io/ipfs/${cid}`
+      updateFileUrl(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
+  return (
+    <div className="App">
+      <h1>IPFS Example</h1>
+      <input
+        type="file"
+        onChange={onChange}
+      />
+      {
+        fileUrl && (
+          <img src={fileUrl} width="600px" />
+        )
+      }
+    </div>
+  );
+}
+
+export default App
